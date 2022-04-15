cc6b1c783b1c059562bd3b2b8bc87b1766eda7e3
AA Student <appacademy@Mini-10.local>
2022-04-14 17:34:23 -0700

    Complete Into to JS

--- introToCB.js
+++ introToCB.js
@@ -195,6 +195,54 @@ class Neuron {
     }
 }
 
-neuron = new Neuron();
-neuron.fire();
-neuron.fire();
+// neuron = new Neuron()
+
+class SearchBar {
+    constructor() {
+        this.query = "";
+
+        this.type = this.type.bind(this);
+        this.search = this.search.bind(this);
+    }
+
+    type(letter) {
+        this.query += letter;
+        this.search();
+    }
+
+    search() {
+        console.log(`searching for ${this.query}`);
+    }
+}
+
+const searchBar = new SearchBar();
+
+const queryForHelloWorld = () => {
+    searchBar.type("h");
+    searchBar.type("e");
+    searchBar.type("l");
+    searchBar.type("l");
+    searchBar.type("o");
+    searchBar.type(" ");
+    searchBar.type("w");
+    searchBar.type("o");
+    searchBar.type("r");
+    searchBar.type("l");
+    searchBar.type("d");
+};
+
+// queryForHelloWorld();
+
+Function.prototype.myDebounce = function (interval) {
+    let oldThis = this;
+    let timeout;
+
+    return function() {
+        setTimeout(function() {
+            oldThis();
+        }, interval);
+    }
+}
+
+searchBar.search = searchBar.search.myDebounce(500);
+queryForHelloWorld;
